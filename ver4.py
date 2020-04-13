13691f10bd120f5393519b0387670217d43d2061
Manika <manikamittal2898>
2020-04-13 17:53:23 +0530

    xml to csv conversion

--- ver4.py
+++ ver4.py
@@ -1,8 +1,9 @@
 import pandas as pd 
+import numpy as np
 import xml.etree.ElementTree as et 
 import csv
 
-xtree = et.parse("/home/manika/Desktop/padhai/IR/small_sample.xml")
+xtree = et.parse("\\Users\\User\\Desktop\\study material\\SEM 6\\INFORMATION RETRIEVAL\\CommunityQuestionAnswering\\try2.xml")
 root = xtree.getroot()
 thread=[]
 # print(root.tag)
@@ -13,8 +14,10 @@ for item in root.iter('vespaadd'):
     ques={}
     for child in root1.findall('document'):
         ques['subject']=child.find('subject').text.encode('utf-8')
-        ques['content']=child.find('content').text.encode('utf-8')
-        ques['bestanswer']=child.find('bestanswer').text.encode('utf-8')
+        if child.find('content') is not None:
+            ques['content']=child.find('content').text.encode('utf-8')
+        if child.find('bestanswer') is not None:
+            ques['bestanswer']=child.find('bestanswer').text.encode('utf-8')
         root2=et.Element('root1')
         root2=child.find('nbestanswers')
  # print(root2.tag)
@@ -26,14 +29,14 @@ for item in root.iter('vespaadd'):
             ques['ans'+str(c)]='None'
             c+=1
     thread.append(ques)
-for x in range(len(thread)): 
-    print thread[x]
-    print ' \n'
+# for x in range(len(thread)): 
+#     print (thread[x])
+#     print (' \n')
 
 fields = ["subject", "content", "bestanswer","ans0","ans1","ans2","ans3","ans4","ans5","ans6","ans7","ans8","ans9"]
 
 # writing to csv file 
-with open("dataset1.csv", 'w') as csvfile: 
+with open("dataset2.csv", 'w') as csvfile: 
 
 # creating a csv dict writer object 
     writer = csv.DictWriter(csvfile, fieldnames = fields) 
