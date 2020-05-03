009157e950b1ef98258ec9f58e5afd1b52f5af2a
Manika <manikamittal2898@gmail.com>
2020-05-03 20:44:21 +0530

    Preprocessing dataset script

---
+++ finaledit.py
@@ -0,0 +1,53 @@
+import gensim
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+
+np.random.seed(2018)
+import nltk
+nltk.download('wordnet')
+
+import csv
+import openpyxl 
+
+wb = openpyxl.Workbook() 
+
+sheet = wb.active 
+with open("/home/manika/Desktop/padhai/IR/Dataset1.csv") as csvDataFile:
+    data = [row for row in csv.reader(csvDataFile)]
+
+lemmatizer= WordNetLemmatizer()
+def lemmatize_stemming(text):
+    return lemmatizer.lemmatize(text) 
+def preprocess(text):
+    # text.replace("<br/>","")
+    # text.replace("\n","")
+    string = ' '
+    for token in gensim.utils.simple_preprocess(text):
+
+        if token not in gensim.parsing.preprocessing.STOPWORDS:
+            string+= " " + (lemmatize_stemming(token))
+    return string
+
+flag=-1
+
+for rows in data:
+    flag+=1
+    flag2=-1
+    for columns in rows:
+        flag2+=1
+        doc_sample = data[flag][flag2]
+        print('original document: ')
+        # doc_sample=doc_sample.replace("<br />","$")
+        # doc_sample=doc_sample.replace("\n","$")
+        # doc_sample=doc_sample.strip("<br />")
+        # doc_sample=doc_sample.strip("\n")
+        # doc_sample = " ".join(doc_sample.splitlines())
+        print(doc_sample)
+        print('tokenized and lemmatized document: ')
+        print(preprocess(doc_sample))
+        c1 = sheet.cell(row = flag+1, column = flag2+1) 
+  
+        # writing values to cells 
+        c1.value = (preprocess(doc_sample))
+wb.save(r"/home/manika/Desktop/padhai/IR/file.xlsx")
+
