d23b64ed3c96674228bd38039af456880b75a2e9
twinklethecoder <manikamittal2898@gmail.com>
2020-05-05 01:21:08 +0530

    version 3 of topicmodelling

--- topicmodel.py
+++ topicmodel.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from numpy import array
 import nltk
 
 import gensim
@@ -8,26 +8,15 @@ from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 
 import pyLDAvis
-import pyLDAvis.gensim  # don't skip this
+import pyLDAvis.gensim 
 import matplotlib.pyplot as plt
-# matplotlib inline
-# from imp import reload
 
 data = pd.read_csv('/home/manika/Desktop/padhai/IR/hojaabhai.csv', error_bad_lines=False)
-# data.head()
-# data_text = data[['subject']]
-# data_text['index'] = data_text.index
-# documents = data_text
-# print(len(documents))
-# print(data[:5])
-# print(data.iloc[:]['subject'])
-# print(data.keys())
+
 data['question'] = data['subject'].str.cat(data['content'], sep ="") 
-# print(data.keys())
-# print(data['question'])
+
 data['tokens'] = data.apply(lambda row: nltk.word_tokenize(row['question']), axis=1)
-# print(data['tokens'])
-# print(data)
+
 dictionary = gensim.corpora.Dictionary(data['tokens'])
 # count = 0
 # for k, v in dictionary.iteritems():
@@ -51,12 +40,56 @@ print(len(dictionary))
 # lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
 # for idx, topic in lda_model.print_topics(-1):
 #     print('Topic: {} \nWords: {}'.format(idx, topic))
+
+
+
+
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model = gensim.models.LdaMulticore(corpus_tfidf, num_topics=num_topics, id2word=dictionary, passes=10, workers=4)
+        # model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=id2word)
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+model_list, coherence_values = compute_coherence_values(dictionary=dictionary, corpus=corpus_tfidf, texts=data['tokens'], start=2, limit=40, step=2)
+
+# Show graph
+limit=40
+start=2
+step=2
+x = range(start, limit, step)
+plt.plot(x, coherence_values)
+plt.xlabel("Num Topics")
+plt.ylabel("Coherence score")
+plt.legend(("coherence_values"), loc='best')
+plt.show()
+
 lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=8, id2word=dictionary, passes=10, workers=4)
 for idx, topic in lda_model_tfidf.print_topics(-1):
     print('Topic: {} Word: {}'.format(idx, topic))
 for index, score in sorted(lda_model_tfidf[bow_corpus[0]], key=lambda tup: -1*tup[1]):
     print("\nScore: {}\t \nTopic: {}".format(score, lda_model_tfidf.print_topic(index, 8)))
-
 
 coherence_model_lda = CoherenceModel(model=lda_model_tfidf, texts=data['tokens'],dictionary=dictionary, coherence='c_v')
 coherence_lda = coherence_model_lda.get_coherence()
